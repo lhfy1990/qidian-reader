5841a2b6521842796f4ebcd4a75d77ff49857215
LHFY <38107178+lhfy1990@users.noreply.github.com>
2018-02-23 08:50:22 -0500

    Language switched to Chinese; Favicon added; Layout adjusted

--- models/bookcase.js
+++ models/bookcase.js
@@ -3,7 +3,7 @@ var bookcaseModel = function(connection) {
     var bookModel = mongoose.model('Book');
 
     var bookcaseSchema = mongoose.Schema({
-        name: { type: String, required: true, trim: true, default: 'New Bookcase' },
+        name: { type: String, required: true, trim: true, default: '新书架' },
         books: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }]
     }, { collection: 'bookcases' });
 
